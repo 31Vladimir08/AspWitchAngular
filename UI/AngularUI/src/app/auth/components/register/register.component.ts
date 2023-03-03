47d73376f34b71ffe2504d60e025be64cf5a5867
Vladimir <vladimir86udm@gmail.com>
2023-03-04 01:05:11 +0400

    RegistrationComponent

--- UI/AngularUI/src/app/auth/components/register/register.component.ts
+++ UI/AngularUI/src/app/auth/components/register/register.component.ts
@@ -1,10 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'mc-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+    form: FormGroup
 
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.initializeForm()
+    }
+
+    initializeForm(): void {
+        this.form = this.fb.group({
+            email: '',
+            userName: ['', Validators.required],
+            displayName : '',
+            password: '',
+            repeatPassword: '',
+            isAgryAllVtatements: false
+        })
+    }
+
+    onSubmit(): void {
+        console.log(this.form.value)
+    }
 }
