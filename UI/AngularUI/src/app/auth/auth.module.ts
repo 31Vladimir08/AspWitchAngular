02d25113464228caabeba7d273245dccee105abb
Vladimir <vladimir86udm@gmail.com>
2023-03-04 16:39:44 +0400

    Added registerService

--- UI/AngularUI/src/app/auth/auth.module.ts
+++ UI/AngularUI/src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from "@ngrx/store";
 
 import { RegisterComponent } from "./components/register/register.component";
+import { AuthService } from "./services/auth.service";
 import { reducers } from "./store/reducers";
 
 const routes: Routes = [
@@ -21,7 +22,8 @@ const routes: Routes = [
         ReactiveFormsModule,
         StoreModule.forFeature('auth', reducers)
     ],
-    declarations: [RegisterComponent]
+    declarations: [RegisterComponent],
+    providers: [AuthService]
 })
 export class AuthModule {
 
