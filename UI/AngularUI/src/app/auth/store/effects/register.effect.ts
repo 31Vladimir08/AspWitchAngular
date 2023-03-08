8de89f65d96b79ab4b6b4e3570dd0982b2b0a6fe
Vladimir <vladimir86udm@gmail.com>
2023-03-08 12:35:12 +0400

    Finished RegisterComponent

--- UI/AngularUI/src/app/auth/store/effects/register.effect.ts
+++ UI/AngularUI/src/app/auth/store/effects/register.effect.ts
@@ -1,8 +1,11 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of } from "rxjs";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
+
+import { PersistanceService } from "src/app/shared/services/persistance.service";
 import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
 import { AuthService } from "../../services/auth.service";
 import { registerAction, registerFailureAction, registerSuccessAction } from "../actions/register.action";
@@ -15,6 +18,8 @@ export class RegisterEffect {
             switchMap(({request}) => {
                 return this.authService.register(request).pipe(
                     map((currentUser: CurrentUserInterface) => {
+                        //window.localStorage.setItem('accessToken', currentUser.token)
+                        this.persistanceService.set('accessToken', currentUser.token)
                         return registerSuccessAction({currentUser})
                     }),
                     catchError((errorResponse: HttpErrorResponse) => {
@@ -26,5 +31,21 @@ export class RegisterEffect {
         )
     )
 
-    constructor(private actions$: Actions, private authService: AuthService) {}
+    redirectAfterSubmit$ = createEffect(() => this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => {
+            this.router.navigateByUrl('/')
+        })
+    ),
+    {dispatch: false}
+    )
+
+    constructor(
+        private actions$: Actions, 
+        private authService: AuthService,
+        private persistanceService: PersistanceService,
+        private router: Router
+    ) {
+
+    }
 }
